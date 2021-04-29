4cc35780d9ea0f4af6198dd94519c5689c0237ac
teqn99 <teqn99@gmail.com>
2021-04-29 23:32:06 +0900

    updated

--- this_is_coding_test/DFS_BFS/경쟁적_전염.py
+++ this_is_coding_test/DFS_BFS/경쟁적_전염.py
@@ -1,51 +1,52 @@
-import copy
-
-N, K = map(int, input().split())
-matrix = [list(map(int, input().split())) for _ in range(N)]
-S, X, Y = map(int, input().split())
-time = 0
-compare_mat = copy.deepcopy(matrix)
-
-# 상, 하, 좌, 우
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-while time != S:
-    for i in range(N):
-        for j in range(N):
-            virus = matrix[i][j]
-            for k in range(4):
-                nx = i + dx[k]
-                ny = j + dy[k]
-                if 0 <= nx < N and 0 <= ny < N:
-                    if matrix[nx][ny] == 0 and compare_mat[nx][ny] == 0:
-                        compare_mat[nx][ny] = virus
-    matrix = copy.deepcopy(compare_mat)
-    time += 1
-
-print(matrix[X-1][Y-1])
-
+# import copy
+#
+# N, K = map(int, input().split())
+# matrix = [list(map(int, input().split())) for _ in range(N)]
+# S, X, Y = map(int, input().split())
+# time = 0
+# compare_mat = copy.deepcopy(matrix)
+#
+# # 상, 하, 좌, 우
+# dx = [-1, 1, 0, 0]
+# dy = [0, 0, -1, 1]
+#
+# while time < S:
+#     for i in range(N):
+#         for j in range(N):
+#             virus = matrix[i][j]
+#             for k in range(4):
+#                 nx = i + dx[k]
+#                 ny = j + dy[k]
+#                 if 0 <= nx < N and 0 <= ny < N:
+#                     if matrix[nx][ny] == 0 and compare_mat[nx][ny] == 0:
+#                         compare_mat[nx][ny] = virus
+#     matrix = copy.deepcopy(compare_mat)
+#     time += 1
+#
+# print(matrix)
+# print(matrix[X-1][Y-1])
+# 이 경우는 바이러스가 번호순으로 증식하지 않을 수 있다... -> 틀린 경우
+# 바이러스가 번호순으로 증식하는 방법을 적용해야 한다.
 #-------------------------------------------------------------------------
 
 from collections import deque
 
-n, k = map(int, input().split())
-test = []  # 전체 맵 정보 담는 리스트
+N, K = map(int, input().split())
+matrix = []  # 전체 맵 정보 담는 리스트
 data = []  # 바이러스 정보를 담는 리스트
-for i in range(n):
-    test.append(list(map(int, input().split())))
-    for j in range(n):
-        if test[i][j] != 0:
-            data.append((test[i][j], i, j, 0))  # 바이러스 숫자, x위치, y위치, 시간을 입력
+for i in range(N):
+    matrix.append(list(map(int, input().split())))
+    for j in range(N):
+        if matrix[i][j] != 0:
+            data.append((matrix[i][j], i, j, 0))  # 바이러스 숫자, x위치, y위치, 시간을 입력
 S, X, Y = map(int, input().split())
 
 # 상, 하, 좌, 우
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-data.sort()  # 바이러스는 번호가 낮은 바이러스부터 증식
+data.sort()  # 바이러스는 번호가 낮은 바이러스부터 증식 - while문에서는 이미 정렬해준 순으로 돌기 때문에 sort는 여기서만 필요
 q = deque(data)
-
 while q:
     virus, x, y, time = q.popleft()
     if time == S:
@@ -53,9 +54,9 @@ while q:
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
-        if 0 <= nx < n and 0 <= ny < n:
-            if test[nx][ny] == 0:
-                test[nx][ny] = virus
-                q.append((virus, nx, ny, time+1))
+        if 0 <= nx < N and 0 <= ny < N:
+            if matrix[nx][ny] == 0:
+                matrix[nx][ny] = virus
+                q.append((virus, nx, ny, time+1))  # q가 한바퀴 돌아야 차례가 오므로 1초가 지나는 경우이다.
 
-print(test[X-1][Y-1])
+print(matrix[X-1][Y-1])
