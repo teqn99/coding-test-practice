c59d31170e7a8adb6bec982782b3fda54fc99892
taekyun <teqn99@gmail.com>
2021-08-26 17:43:14 +0900

    0826 solved

--- SWEA/0826/미로1.py
+++ SWEA/0826/미로1.py
@@ -23,3 +23,38 @@ for tc in range(10):
 
     ans = find_road(a, b)
     print(f'#{N} {ans}')
+
+
+# ============================================================================
+# 다른 풀이
+from collections import deque
+
+
+def bfs(maze, x, y):  # x, y: 시작점
+    visited = [[0] * 16 for _ in range(16)]
+    q = deque()
+    q.append([x, y])
+    while q:
+        cx, cy = q.popleft()
+        visited[cy][cx] = 1
+        if maze[cy][cx] == '3':  # 도착점 발견한 경우, 1 리턴
+            return 1
+
+        for dx, dy in [[0, 1], [0, -1], [1, 0], [-1, 0]]:  # 이동 방향 탐색
+            nx, ny = cx + dx, cy + dy  # 범위를 벗어날 수 없으므로, 범위는 고려 X
+            if not visited[ny][nx] and maze[ny][nx] != '1':
+                q.append([nx, ny])
+
+    return 0  # 도착점 찾지 못한 경우 0 리턴
+
+
+for _ in range(1, 11):
+    tc = int(input())
+    maze = [input() for _ in range(16)]
+
+    for i in range(16):  # 시작점 탐색
+        if maze[i].count('2'):
+            sx, sy = maze[i].index('2'), i
+            break
+
+    print(f'#{tc} {bfs(maze, sx, sy)}')
