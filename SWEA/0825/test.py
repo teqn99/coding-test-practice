fe2ec3a7748497b3fc4a0fe8d128d3ca999ccaad
taekyun <teqn99@gmail.com>
2021-08-26 10:07:23 +0900

    updated

--- SWEA/0825/test.py
+++ SWEA/0825/test.py
@@ -12,7 +12,6 @@ for tc in range(T):
 
 # ===========================================
 # 밑의 풀이는 잘 진행되지 않음.
-# 뇌좀 깨우고 다시 생각해볼 것
 
 T = int(input())
 for tc in range(T):
@@ -33,3 +32,51 @@ for tc in range(T):
     print('#{0} {1:.6f}'.format(tc+1, max_v))
 
 print(2.7 * (3 - 2.7*2) * (3 - 2.7*2))
+
+# ==================================================
+# 완성 풀이 2
+"""
+0 < h < min(X, Y) / 2
+A = (X - 2h) * (Y - 2h) * h
+
+h 초기값: min(X, Y) / 4로 설정
+delta = h / 2
+
+A1 = (X - 2(h+delta))(Y - 2(h+delta))(h+delta)
+A2 = (X - 2(h-delta))(Y - 2(h-delta))(h-delta)
+
+|A1 - A2| < 원하는 범위 인지를 확인하는 것이 중요!!
+->
+    if abs(A1 - A2) < 1/(10**8):  # (= 1e-8)
+        같은 걸로 칠게!
+    elif A1 > A2:
+        h += delta
+    elif A1 < A2:
+        h -= delta
+    else:
+    
+    
+a = 1e-6
+b = 1e6  # 10**6과 동일
+print(f'{a:.6f')
+"""
+T = int(input())
+for tc in range(1, T+1):
+    X, Y = map(int, input().split())
+    h = min(X, Y) / 4                      # 0 < h < min(X, Y) / 2 -> h의 중간값은 min(X, Y) / 4이다.
+    d = h / 2
+    ans = 0
+
+    while True:
+        A1 = (X - 2 * (h + d)) * (Y - 2 * (h + d)) * (h + d)
+        A2 = (X - 2 * (h - d)) * (Y - 2 * (h - d)) * (h - d)
+        if abs(A1 - A2) < 1e-8:
+            ans = A1
+            break
+        elif A1 > A2:
+            h += d
+        elif A1 < A2:
+            h -= d
+        d /= 2
+
+    print(f'#{tc} {ans:.6f}')
