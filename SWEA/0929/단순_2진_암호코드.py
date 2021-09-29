9cdd9163a27d18e17fe58089c43254447f501e58
taekyun <teqn99@gmail.com>
2021-09-29 17:37:06 +0900

    0929 solved

--- SWEA/0929/단순_2진_암호코드.py
+++ SWEA/0929/단순_2진_암호코드.py
@@ -32,3 +32,49 @@ for tc in range(1, int(input())+1):
         print(f'#{tc} {sum(map(int, list(code)))}')
     else:
         print(f'#{tc} 0')
+
+
+# 다른 풀이
+code_dic = {
+    '0001101': '0',
+    '0011001': '1',
+    '0010011': '2',
+    '0111101': '3',
+    '0100011': '4',
+    '0110001': '5',
+    '0101111': '6',
+    '0111011': '7',
+    '0110111': '8',
+    '0001011': '9'
+}
+
+
+def validate(n):
+    temp = 0
+    check = 0
+    for i, x in enumerate(n):
+        ix = int(x)
+        temp += ix
+        if i % 2:
+            check += ix
+            if i == 7:
+                return temp if not check % 10 else 0
+        else:
+            check += ix * 3
+
+
+for tc in range(1, int(input()) + 1):
+    N, M = map(int, input().split())
+    codes = [input() for _ in range(N)]
+
+    for i in range(N):
+        if '1' in codes[i]:  # 유효한 코드가 포함되어 있으면
+            j = codes[i].rindex('1')
+            code_bin = codes[i][j - 55:j + 1]
+            break
+
+    code = ''
+    for i in range(8):  # 10진수로 변환
+        code += code_dic[code_bin[i * 7:i * 7 + 7]]
+
+    print(f'#{tc} {validate(code)}')
