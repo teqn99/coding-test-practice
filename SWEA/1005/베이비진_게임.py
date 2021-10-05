1405e9ce03b63549d675af1ceede2ca3e4ee2d3e
taekyun <teqn99@gmail.com>
2021-10-05 17:38:34 +0900

    1005 solved

--- SWEA/1005/베이비진_게임.py
+++ SWEA/1005/베이비진_게임.py
@@ -43,3 +43,37 @@ for tc in range(1, int(input())+1):
                 break
 
     print(f'#{tc} {result}')
+
+
+# 다른 풀이
+def babygin(num, cnt):  # 카드숫자, 카운팅리스트
+    cnt[num] += 1
+    tmp = 0
+    i = 0
+    while i < 8:
+        if cnt[i] >= 3:  # triplet check
+            tmp = 1
+            break
+        if cnt[i] and cnt[i + 1] and cnt[i + 2]:  # run check
+            tmp = 1
+            break
+        i += 1
+    if tmp == 1:
+        return True
+
+
+T = int(input())
+for tc in range(1, T + 1):
+    cards = list(map(int, input().split()))
+    p1, p2 = [0] * 10, [0] * 10
+
+    ans = 0
+    for i in range(0, len(cards), 2):
+        if babygin(cards[i], p1):
+            ans = 1
+            break
+        if babygin(cards[i + 1], p2):
+            ans = 2
+            break
+
+    print("#{} {}".format(tc, ans))
